8d839480d342189d8cae9ff886b945044006bbe4
Diegoarraez10 <diegoarraez10@hotmail.com>
2023-05-18 20:07:44 +0000

    fixed details button

--- src/front/js/component/productGrid.jsx
+++ src/front/js/component/productGrid.jsx
@@ -2,30 +2,48 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import wii from "../../img/imgWii.jpg";
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import "../../styles/productCard.css"
+import "../../styles/productCard.css";
 
-
-
-
-function Product({product}) {
-  const {store,actions}= useContext(Context)
+function Product({ product }) {
+  const { store, actions } = useContext(Context);
   return (
-    <Col lg={3} md={3} xs={12}  key={product.id}>
-      <Card className= "my-3 cardgrid">
-        <Card.Img variant="top" src={product.product_img} className="card-img-fixed" />
+    <Col lg={3} md={3} xs={12} key={product.id}>
+      <Card className="my-3 cardgrid">
+        <Card.Img
+          variant="top"
+          src={product.product_img}
+          className="card-img-fixed"
+        />
         <Card.Body>
           <Card.Title>{product.name}</Card.Title>
           <Card.Text>{product.status}</Card.Text>
           <Card.Text>
-            <strong>Price: </strong>{product.price}
+            <strong>Price: </strong>
+            {product.price}
           </Card.Text>
           <Row xs={1} md={1} lg={1} className="g-3">
-            <Link to={`/productDetails/${product.id}`} variant="primary">Details</Link>
-            <Button variant="primary" onClick={()=>actions.handleTransaccion(product.id, product.status)}>Buy now!</Button>
+            <Button variant="primary">
+              <Link
+                to={`/productDetails/${product.id}`}
+                variant="primary"
+                style={{ color: "white", textDecoration: "none" }}
+              >
+                Details
+              </Link>
+            </Button>
+
+            <Button
+              variant="primary"
+              onClick={() =>
+                actions.handleTransaccion(product.id, product.status)
+              }
+            >
+              Buy now!
+            </Button>
           </Row>
         </Card.Body>
       </Card>
