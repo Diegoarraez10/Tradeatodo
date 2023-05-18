cff06f6ea0140eddf8d62331211864191bf10924
jgomez2011 <jesus.gomez0911@gmail.com>
2023-05-18 20:28:51 +0000

    about jesus

--- src/front/js/pages/about.jsx
+++ src/front/js/pages/about.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import diegoPP from "../../img/diego_pp.jpg"
-
+import profile from "../../img/profile.jpg"
 
 export const About= () => {
 
@@ -16,9 +16,9 @@ export const About= () => {
 
             <div class="row gx-1">
                 <div class="col-12 "> <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod rerum dicta facere eaque aut! Dolores libero modi vel corrupti excepturi eveniet reiciendis cumque? Sint nostrum consequuntur perferendis, explicabo odit quas. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita error, a aliquam, soluta excepturi delectus accusantium optio esse nam placeat ullam molestiae reiciendis cumque minima fugit tempore? Dolore, accusantium exercitationem?</p></div>
-               
+              
                 <div class="col-12 my-3">
-             
+      
         <div className="container cards ">
           <div className="row d-flex justify-content-center  ">
             <div className="col col-12 col-lg-3">
@@ -33,10 +33,11 @@ export const About= () => {
             </div>     
             <div className="col col-12 col-lg-3">
             <div className="card " >
-                <img src="https://placehold.co/600x400" className="card-img-top" alt="..."/>
+                <img src={profile} className="card-img-top" alt="..."/>
                 <div class="card-body">
-                    <h5 class="card-title">Nombre</h5>
-                    <p class="card-text">Datos personales</p>
+                    <h5 class="card-title">Jesus Gomez</h5>
+                    <p class="card-text"> Tengo 22 años, Fullstack developer, Me gusta leer, los videojuegos, el cine, entre otras cosas,
+                    siempre me ha llamado la atencion la programacion y gracias a este curso ha despertado mas la curiosidad por todo este tema y las ganas de seguir aprendiendo </p>
                 </div>
 
               </div>
